c207dfe64ae7a1bbd219ccc822ae6d3782420880
salauddin-saife <salauddin15saife@gmail.com>
2025-02-02 04:02:33 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div> <h1 class="text-3xl font-bold underline">
-    Hello world! ok
-  </h1></div>
+    <div> 
+      <navbar/>
+    </div>
   )
 }
 
