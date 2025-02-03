1f2c2b802330da929b98e1b29a0beedd38899b13
salauddin-saife <salauddin15saife@gmail.com>
2025-02-03 20:34:32 +0600

    banner update

--- src/Layout/Banner.jsx
+++ src/Layout/Banner.jsx
@@ -5,8 +5,17 @@ const Banner = () => {
   return (
     <div>
       <section className="bg-[#F8F8F8]">
-        <Contain>
-            Hello
+        <Contain className="h-[886px]">
+            <div className=" ">
+              <h1 className="font-poppins text-[65px] w-[573px] font-semibold">Start Exploring to Find Inner Peace
+              by Traveling</h1>
+            </div>
+            <div>
+              <p>Lost yourself with Vacation and Travel into the place that you never visit before. We have thousand places in our database for you.</p>
+            </div>
+            <div>
+              <input type="text" />
+            </div>
         </Contain>
       </section>
     </div>
