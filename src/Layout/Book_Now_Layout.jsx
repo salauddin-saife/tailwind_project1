362679fdaf2099e5389a6e03bfe67f2788b89585
salauddin-saife <salauddin15saife@gmail.com>
2025-02-07 17:04:56 +0600

    book now update

--- src/Layout/Book_Now_Layout.jsx
+++ src/Layout/Book_Now_Layout.jsx
@@ -1,30 +1,58 @@
 import React from "react";
 import Book_Now_Card from "../components/Book_Now_Card";
 import Flex from "../components/Flex";
+import Paris from "../assets/Paris.png";
+import Venice from "../assets/Venice.png";
+import Santorini from "../assets/Santorini.png";
 
 const Book_Now_Layout = () => {
   return (
     <section className="max-w-[1920px] flex justify-around items-center align-middle gap-x-6 pt-[100px]">
-
-<div className=" w-full flex justify-around items-center align-middle gap-x-6">
-      <div className="flex justify-around items-center align-middle gap-x-6">
-        <Flex className={"flex justify-around items-center align-middle gap-x-6"}>
-          <div className=" 
-          flex justify-around items-center align-middle gap-x-6">
-            <Book_Now_Card />
-            <Book_Now_Card />
-            <Book_Now_Card />
-            <Book_Now_Card />
-            <Book_Now_Card />
-          </div>
-        </Flex>
+      <div className=" w-full flex justify-around items-center align-middle gap-x-6">
+        <div className="flex justify-around items-center align-middle gap-x-6">
+          <Flex
+            className={"flex justify-around items-center align-middle gap-x-6"}
+          >
+            <div
+              className=" 
+          flex justify-around items-center align-middle gap-x-6"
+            >
+              <Book_Now_Card
+                img_src={Paris}
+                Book_now_h1={"Paris, France"}
+                Book_now_h2={"$150"}
+                Book_now_h3={" / Person"}
+                Book_now_p={
+                  "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua minim veniam"
+                }
+                Book_now_btn_text={"Book Now"}
+              />
+              <Book_Now_Card
+                img_src={Venice}
+                Book_now_h1={"Paris, France"}
+                Book_now_h2={"$150"}
+                Book_now_h3={" / Person"}
+                Book_now_p={
+                  "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua minim veniam"
+                }
+                Book_now_btn_text={"Book Now"}
+              />
+              <Book_Now_Card
+                img_src={Santorini}
+                Book_now_h1={"Paris, France"}
+                Book_now_h2={"$150"}
+                Book_now_h3={" / Person"}
+                Book_now_p={
+                  "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua minim veniam"
+                }
+                Book_now_btn_text={"Book Now"}
+              />
+            </div>
+          </Flex>
+        </div>
       </div>
-    </div>
     </section>
-   
   );
 };
 
 export default Book_Now_Layout;
-
-
--- src/components/Book_Now_Card.jsx
+++ src/components/Book_Now_Card.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React from "react";
+import Image from "./Image";
+import { FaStar } from "react-icons/fa6";
+import Button from "./Button";
+import { FaStarHalfAlt } from "react-icons/fa";
 
-const Book_Now_Card = () => {
+const Book_Now_Card = ({img_src,Book_now_h1, Book_now_h2,Book_now_p, Book_now_btn_text,Book_now_h3}) => {
   return (
-    <div>
-        <div className='w-[370px] h-[592px] bg-amber-200  '>
-            
-
+    <div className="">
+      <div className="w-[370px] h-[592px] bg-white rounded-2xl shadow-[0px_0px_7px_-4px_rgba(0,0,0,0.75)] hover:shadow-[0px_0px_25px_-4px_rgba(0,0,0,0.75)] duration-300 pointer-events-auto">
+        <div className="pt-[15px]">
+          <Image className={"rounded-2xl scale-[85%] hover:scale-[90%] duration-700"} src={img_src} />
         </div>
+        <div className="flex gap-x-3 text-[#FF8B00] pl-[30px] pt-[20px]">
+          <FaStar />
+          <FaStarHalfAlt />
+          <FaStarHalfAlt />
+          <FaStarHalfAlt />
+        </div>
+        <div className="pl-[30px] font-poppins flex-row gap-y-6 pt-[10px]">
+          <div className="text-[25px] font-medium leading-[ 24px] tracking-[0.2px] text-left align-top text-font_primary gap-6 ">
+            <h1>{Book_now_h1}</h1>
+          </div>
+          <div className="w-[325px] font-normal text-[16px] leading-[30px] tracking-[] align-top text-font_paragraph pt-[10px]">
+            <p>
+            {Book_now_p}
+            </p>
+          </div>
+          <div className="pt-[20px] flex align-top text-left ">
+            <h2 className="font-semibold text-[20px] leading-[24px] tracking-[0.2px]">{Book_now_h2}</h2>
+            <h3>{Book_now_h3}</h3>
+          </div>
+          <div className="pt-[50px]">
+            <Button text={Book_now_btn_text}/>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Book_Now_Card
+export default Book_Now_Card;
