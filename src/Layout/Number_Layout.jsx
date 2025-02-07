893ecf050afd0f407ece3f281378bb09536b6343
salauddin-saife <salauddin15saife@gmail.com>
2025-02-08 05:09:27 +0600

    update

--- src/Layout/Number_Layout.jsx
+++ src/Layout/Number_Layout.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import Number_Box from '../components/Number_Box'
+import React from "react";
+import Number_Box from "../components/Number_Box";
+import Contain from "../components/Contain";
+import { BiSolidHandRight } from "react-icons/bi";
+import Button from "../components/Button";
 
 const Number_Layout = () => {
   return (
-    <div>
-        <Number_Box/>
-    </div>
-  )
-}
+    <>
+      <section className="bg-[#1E1E1E]/5 mt-[90px]">
+        <Contain className={""}>
+          <div className="align-top text-left flex">
+            <div className="w-6/12">
+              <div className="w-[418px] pt-[100px] text-font.primary text-[40px] font-semibold tracking-[0.2px] font-poppins">
+                <h1>Go Travel, Discover Remember Us</h1>
+              </div>
+              <div className="">
+                <div className="w-[516px] text-font_paragraph text-[16px] pt-[20px] font-normal leading-[30px] font-poppins">
+                  <p>
+                    We are self-service data analytics software that lets you
+                    create visually appealing data visualizations and insightful
+                    dashboards in minutes.
+                  </p>
+                </div>
+                <div className="flex pt-[20px] justify-start items-center align-middle gap-x-6  text-font_paragraph text-[16px] font-normal leading-[30px] font-poppins ">
+                  <BiSolidHandRight className="text-web_green" />
+                  <p className="w-[338px]">
+                    We are self-service data software visually appealing data
+                    visualizations.
+                  </p>
+                </div>
+                <div className="flex pt-[10px] justify-start items-center align-middle gap-x-6  text-font_paragraph text-[16px] font-normal leading-[30px] font-poppins ">
+                  <BiSolidHandRight className="text-web_green" />
+                  <p className="w-[338px]">
+                    We are self-service data software visually appealing data
+                    visualizations.
+                  </p>
+                </div>
+                <div className="flex  pt-[10px] justify-start items-center align-middle gap-x-6  text-font_paragraph text-[16px] font-normal leading-[30px] font-poppins ">
+                  <BiSolidHandRight className="text-web_green" />
+                  <p className="w-[338px]">
+                    We are self-service data software visually appealing data
+                    visualizations.
+                  </p>
+                </div>
+              </div>
+              <div className="pt-[50px]">
+                <Button text={"Discover Place"} />
+              </div>
+            </div>
+            <div className="w-6/12">
+              <div className="flex gap-x-8 pt-[60px]">
+                <div>
+                  <Number_Box />
+                </div>
+                <div>
+                  <Number_Box />
+                </div>
+              </div>
+              <div className="flex gap-x-8 pt-[30px] pb-[60px] ">
+                <div>
+                  <Number_Box />
+                </div>
+                <div>
+                  <Number_Box />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Contain>
+      </section>
+    </>
+  );
+};
 
-export default Number_Layout
+export default Number_Layout;
--- src/components/Number_Box.jsx
+++ src/components/Number_Box.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React from "react";
 import { IoMdContact } from "react-icons/io";
+import Contain from "./Contain";
 
-
-const Number_Box = () => {
+const Number_Box = ({className}) => {
   return (
-    <div>
-        <div className='w-[275px] h-[325px]'>
-<div>
-<IoMdContact />
-
-</div>
-<div>
-    <div>
-        <h1>500K+</h1>
-    </div>
-    <div>
-        <h2>Satisfied Clients</h2>
-    </div>
-</div>
+    <Contain>
+      <div className={`align-top text-left  pointer-events-auto ${className}`}>
+        <div className="w-[275px] h-[325px] group-hover:text-white bg-white hover:bg-web_green rounded-2xl shadow-[0px_0px_7px_-4px_rgba(0,0,0,0.75)] hover:shadow-[0px_0px_25px_-4px_rgba(0,0,0,0.75)] group duration-300 pointer-events-auto">
+          <div className="pl-[68px] pt-[80px] pointer-events-auto">
+            <IoMdContact className="text-web_green  scale-[5] hover:scale-[6] p-[3px] bg-web_green/10  group-hover:bg-white rounded-[2px] duration-300 group-hover:text-web_green" />
+          </div>
+          <div className="pt-[80px] pl-[30px] pointer-events-auto">
+            <div className="text-font_primary font-poppins text-[40px] font-semibold tracking-[0.2px] group-hover:text-white">
+              <h1>500K+</h1>
+            </div>
+            <div className="text-font_paragraph font-poppins font-medium text-[25px] tracking-[0.2px] group-hover:text-white">
+              <h2>Satisfied Clients</h2>
+            </div>
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
+    </Contain>
+  );
+};
 
-export default Number_Box
+export default Number_Box;
+
